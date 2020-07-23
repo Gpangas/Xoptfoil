6cd90895e55cf43fb3f7d15df1d4f1fc959bd451
r-palmeira-pt <43786228+r-palmeira-pt@users.noreply.github.com>
2020-07-23 20:00:15 +0100

    Started modulization of parametrization functions

--- src/fortran/parametrization.f90
+++ src/fortran/parametrization.f90
@@ -121,103 +121,18 @@ end subroutine create_shape_functions
 !=============================================================================80
 subroutine create_shape(x, modes, shapetype, shape_function)
 
-  use vardef, only : initial_perturb, min_bump_width
-
   double precision, dimension(:), intent(in) :: x, modes
   character(*), intent(in) :: shapetype
   double precision, dimension(:,:), intent(inout) :: shape_function
 
-  integer :: npt, nmodes, i, j, counter1, counter2
-  double precision :: power1, power2, dvscale, st, t1, t2, t1fact, t2fact, pi
-  double precision :: chord, xle, xs
-
-  npt = size(x,1)
-  chord = x(npt) - x(1)
-  xle = x(1)
-
   shape_switch: if (trim(shapetype) == 'naca') then
-
-    nmodes = size(modes,1)
-
-!   Create naca shape functions
-
-    do j = 1, npt
-      xs = (x(j)-xle)/chord
-      shape_function(1,j) = sqrt(xs) - xs
-    end do
-
-    counter1 = 1
-    counter2 = 1
-
-    do i = 2, nmodes
-
-!     Whole-powered shapes
-
-      if (counter2 == 1) then
-
-        power1 = dble(counter1)
-        do j = 1, npt
-          xs = (x(j)-xle)/chord
-          shape_function(i,j) = xs**(power1)*(1.d0 - xs)
-        end do
-        counter2 = 2
-
-!     Fractional-powered shapes
-
-      else
-
-        power1 = 1.d0/dble(counter1 + 2)
-        power2 = 1.d0/dble(counter1 + 1)
-        do j = 1, npt
-          xs = (x(j)-xle)/chord
-          shape_function(i,j) = xs**power1 - xs**power2
-        end do
-        counter2 = 1
-        counter1 = counter1 + 1
-       
-      end if
-
-    end do
-
-!   Normalize shape functions
-
-    do i = 1, nmodes
-      dvscale = 1.d0/abs(maxval(shape_function(i,:)))
-      shape_function(i,:) = shape_function(i,:)*dvscale
-    end do
-
+    
+    call NACA_shape(x, modes, shape_function)
+    
   elseif (trim(shapetype) == 'hicks-henne') then
-      
-    nmodes = size(modes,1)/3
-    t1fact = initial_perturb/(1.d0 - 0.001d0)
-    t2fact = initial_perturb/(10.d0 - min_bump_width)
-    pi = acos(-1.d0)
-
-    do i = 1, nmodes
-
-!     Extract strength, bump location, and width
-
-      counter1 = 3*(i-1)
-      st = modes(counter1+1)
-      t1 = modes(counter1+2)/t1fact
-      t2 = modes(counter1+3)/t2fact
-
-!     Check for problems with bump location and width parameters
-
-      if (t1 <= 0.d0) t1 = 0.001d0
-      if (t1 >= 1.d0) t1 = 0.999d0
-      if (t2 <= 0.d0) t2 = 0.001d0
-
-!     Create shape function
-
-      power1 = log10(0.5d0)/log10(t1)
-      do j = 2, npt-1
-        xs = (x(j)-xle)/chord
-        shape_function(i,j) = st*sin(pi*xs**power1)**t2
-      end do
-
-    end do
-
+    
+    call HH_shape(x, modes, shape_function)
+    
   else
 
     write(*,*)
@@ -229,6 +144,126 @@ subroutine create_shape(x, modes, shapetype, shape_function)
 
 end subroutine create_shape
 
+!=============================================================================80
+!
+! Populates shape function arrays for Hicks-Hene shape functions,
+! number of elements in modes must be a multiple of 3.
+!
+!=============================================================================80
+subroutine HH_shape(x, modes, shape_function)
+
+  use vardef, only : initial_perturb, min_bump_width
+
+  double precision, dimension(:), intent(in) :: x, modes
+  double precision, dimension(:,:), intent(inout) :: shape_function
+
+  integer :: npt, nmodes, i, j, counter1
+  double precision :: power1, st, t1, t2, t1fact, t2fact, pi
+  double precision :: chord, xle, xs
+
+  npt = size(x,1)
+  chord = x(npt) - x(1)
+  xle = x(1)
+
+  
+  nmodes = size(modes,1)/3
+  t1fact = initial_perturb/(1.d0 - 0.001d0)
+  t2fact = initial_perturb/(10.d0 - min_bump_width)
+  pi = acos(-1.d0)
+
+  do i = 1, nmodes
+
+!     Extract strength, bump location, and width
+
+    counter1 = 3*(i-1)
+    st = modes(counter1+1)
+    t1 = modes(counter1+2)/t1fact
+    t2 = modes(counter1+3)/t2fact
+
+!     Check for problems with bump location and width parameters
+
+    if (t1 <= 0.d0) t1 = 0.001d0
+    if (t1 >= 1.d0) t1 = 0.999d0
+    if (t2 <= 0.d0) t2 = 0.001d0
+
+!     Create shape function
+
+    power1 = log10(0.5d0)/log10(t1)
+    do j = 2, npt-1
+      xs = (x(j)-xle)/chord
+      shape_function(i,j) = st*sin(pi*xs**power1)**t2
+    end do
+
+  end do
+end subroutine
+!=============================================================================80
+!
+! Populates shape function arrays for NACA shape functions
+!
+!=============================================================================80
+subroutine NACA_shape(x, modes, shape_function)
+
+  double precision, dimension(:), intent(in) :: x, modes
+  double precision, dimension(:,:), intent(inout) :: shape_function
+
+  integer :: npt, nmodes, i, j, counter1, counter2
+  double precision :: power1, power2, dvscale
+  double precision :: chord, xle, xs
+
+  npt = size(x,1)
+  chord = x(npt) - x(1)
+  xle = x(1)
+
+  nmodes = size(modes,1)
+
+!   Create naca shape functions
+
+  do j = 1, npt
+    xs = (x(j)-xle)/chord
+    shape_function(1,j) = sqrt(xs) - xs
+  end do
+
+  counter1 = 1
+  counter2 = 1
+
+  do i = 2, nmodes
+
+!     Whole-powered shapes
+
+    if (counter2 == 1) then
+
+      power1 = dble(counter1)
+      do j = 1, npt
+        xs = (x(j)-xle)/chord
+        shape_function(i,j) = xs**(power1)*(1.d0 - xs)
+      end do
+      counter2 = 2
+
+!     Fractional-powered shapes
+
+    else
+
+      power1 = 1.d0/dble(counter1 + 2)
+      power2 = 1.d0/dble(counter1 + 1)
+      do j = 1, npt
+        xs = (x(j)-xle)/chord
+        shape_function(i,j) = xs**power1 - xs**power2
+      end do
+      counter2 = 1
+      counter1 = counter1 + 1
+       
+    end if
+
+  end do
+
+!   Normalize shape functions
+
+  do i = 1, nmodes
+    dvscale = 1.d0/abs(maxval(shape_function(i,:)))
+    shape_function(i,:) = shape_function(i,:)*dvscale
+  end do
+
+end subroutine NACA_shape
 !=============================================================================80
 !
 ! Creates an airfoil surface by perturbing an input "seed" airfoil
@@ -244,36 +279,52 @@ subroutine create_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
   character(*), intent(in) :: shapetype
   logical, intent(in) :: symmetrical
 
-  integer :: i, nmodest, nmodesb, npointst, npointsb
-  double precision :: strength
-
   if (trim(shapetype) == 'naca') then
-    nmodest = size(modest,1)
-    nmodesb = size(modesb,1)
+    
+    call NACA_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
+                          zt_new, zb_new, symmetrical)
   else
-    nmodest = size(modest,1)/3
-    nmodesb = size(modesb,1)/3
+    
+    call HH_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
+                          zt_new, zb_new, shapetype, symmetrical)
   end if
+
+end subroutine create_airfoil
+
+!=============================================================================80
+!
+! Creates an airfoil surface by perturbing an input "seed" airfoil
+! Using Hicks-Henne bump functions
+!
+!=============================================================================80
+subroutine HH_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
+                          zt_new, zb_new, shapetype, symmetrical)
+
+  double precision, dimension(:), intent(in) :: xt_seed, zt_seed, xb_seed,     &
+                                                zb_seed
+  double precision, dimension(:), intent(in) :: modest, modesb
+  double precision, dimension(:), intent(inout) :: zt_new, zb_new
+  character(*), intent(in) :: shapetype
+  logical, intent(in) :: symmetrical
+
+  integer :: i, nmodest, nmodesb, npointst, npointsb
+  
+  nmodest = size(modest,1)/3
+  nmodesb = size(modesb,1)/3
+  
   npointst = size(zt_seed,1)
   npointsb = size(zb_seed,1)
 
 ! Create shape functions for Hicks-Henne
 
-  if (trim(shapetype) == 'hicks-henne') then
-    call create_shape_functions(xt_seed, xb_seed, modest, modesb, shapetype,   &
+  call create_shape_functions(xt_seed, xb_seed, modest, modesb, shapetype,   &
                                 first_time=.false.)
-  end if
 
 ! Top surface
 
   zt_new = zt_seed
   do i = 1, nmodest
-    if (trim(shapetype) == 'naca') then
-      strength = modest(i)
-    else
-      strength = 1.d0
-    end if
-    zt_new = zt_new + strength*top_shape_function(i,1:npointst)
+    zt_new = zt_new + top_shape_function(i,1:npointst)
   end do
 
 ! Bottom surface
@@ -281,12 +332,7 @@ subroutine create_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
   if (.not. symmetrical) then
     zb_new = zb_seed
     do i = 1, nmodesb
-      if (trim(shapetype) == 'naca') then
-        strength = modesb(i)
-      else
-        strength = 1.d0   ! Hicks-Henne: strength is part of the shape function
-      end if
-      zb_new = zb_new + strength*bot_shape_function(i,1:npointsb)
+      zb_new = zb_new + bot_shape_function(i,1:npointsb)
     end do
 
 ! For symmetrical airfoils, just mirror the top surface
@@ -297,6 +343,57 @@ subroutine create_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
     end do
   end if
 
-end subroutine create_airfoil
+end subroutine HH_airfoil
 
+!=============================================================================80
+!
+! Creates an airfoil surface by perturbing an input "seed" airfoil
+! Using NACA bump functions
+!
+!=============================================================================80
+  subroutine NACA_airfoil(xt_seed, zt_seed, xb_seed, zb_seed, modest, modesb,  &
+                          zt_new, zb_new, symmetrical)
+
+  double precision, dimension(:), intent(in) :: xt_seed, zt_seed, xb_seed,     &
+                                                zb_seed
+  double precision, dimension(:), intent(in) :: modest, modesb
+  double precision, dimension(:), intent(inout) :: zt_new, zb_new
+  logical, intent(in) :: symmetrical
+
+  integer :: i, nmodest, nmodesb, npointst, npointsb
+  double precision :: strength
+
+  nmodest = size(modest,1)
+  nmodesb = size(modesb,1)
+
+  npointst = size(zt_seed,1)
+  npointsb = size(zb_seed,1)
+
+! Top surface
+
+  zt_new = zt_seed
+  do i = 1, nmodest
+    strength = modest(i)
+    zt_new = zt_new + strength*top_shape_function(i,1:npointst)
+  end do
+
+! Bottom surface
+
+  if (.not. symmetrical) then
+    zb_new = zb_seed
+    do i = 1, nmodesb
+      strength = modesb(i)
+      zb_new = zb_new + strength*bot_shape_function(i,1:npointsb)
+    end do
+
+! For symmetrical airfoils, just mirror the top surface
+
+  else
+    do i = 1, npointsb
+      zb_new(i) = -zt_new(i)
+    end do
+  end if
+  
+  end subroutine NACA_airfoil
+  
 end module parametrization
